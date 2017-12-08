ac5eec147d38a35c654174f0f0614fc91a388152
lzc <1101258845@qq.com>
2017-12-08 15:51:44 +0800

    init

--- example/index.js
+++ example/index.js
@@ -1,59 +1,73 @@
 let  { apiModule, ResourceParams, ApiService} = require('../index');
+//实际项目中你应该使用 let  { apiModule, ResourceParams, ApiService} = require('api-service');
 
+//创建服务类
 @ResourceParams({
-    apiPath: '/app/:aa/:gg',
+    apiPath: '/app/:id',
     actionsToLocalCache: ['get'],
-    cacheKey: 'app.css',
-    identity: '123'
+    cacheKey: 'app.test',
     // disableCache: true
     // actionsToCache: ['get']
 })
 class Api extends ApiService{}
 
-var app = angular.module('app', [apiModule])
-    .config(['localCacheServiceProvider', '$logProvider', function(localCacheServiceProvider, $logProvider){
+//加入‘apiModule’到根模块
+var app = angular.module('app', [apiModule]);
+
+//设置服务器地址
+app.config(['localCacheServiceProvider', '$logProvider', function(localCacheServiceProvider){
     localCacheServiceProvider.setConfig({
         API_SERVER: 'http://localhost:8888',
-        DB_TYPE: 'indexedDB'
+        // DB_TYPE: 'indexedDB'
     });
-    $logProvider.debugEnabled(true);
 }]);
 
+//创建服务
 app.service('Api', Api);
 
+//控制器
 app.controller('myCtrl', ['$scope','Api', 'localCacheService', function($scope, Api, localCacheService) {
+    //设置用户标识
     localCacheService.setIdentity("AAAAA");
+    //查看cacheKey
     console.log(Api.cacheKey);
+
+    //actions: create
     $scope.create = function() {
         Api.create({id:123 , actions: 'create'}, (res) => {
             $scope.code = angular.toJson(res, true);
         });
     };
 
+    //actions: get
     $scope.get = function() {
         Api.get({id:123, actions: 'get'}).$promise.then(res => {
             $scope.code = angular.toJson(res, true);
         });
     };
 
+    //actions: list
     $scope.list = function() {
         Api.list({id:123, actions: 'list'}).$promise.then(res => {
             $scope.code = angular.toJson(res, true);
         });
     };
 
+    //actions: getAll
     $scope.getAll = function() {
         Api.getAll({id:123 , actions: 'getAll'}, (res) => {
             $scope.code = angular.toJson(res, true);
         });
     };
 
+    //actions: update
     $scope.update = function() {
         Api.update({id:123, actions: 'update'}).$promise.then(res => {
             $scope.code = angular.toJson(res, true);
         });
     };
 
+    //actions: delete
     $scope.delete = function() {
         Api.delete({id:123, actions: 'delete'}).$promise.then(res => {
             $scope.code = angular.toJson(res, true);
